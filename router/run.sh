0faec053051afa7849729d8e3af6173333a06898
Wagner Souza <wagnersza@corp.globo.com>
2015-08-13 20:05:03 -0300

    router without consul-template

--- router/run.sh
+++ router/run.sh
@@ -1,3 +1,2 @@
 /etc/init.d/redis-server start
-/etc/init.d/hipache start
-/bin/consul-template -consul consul.service.consul:8500 -template "/tmp/redis.ctmpl:/etc/redis/redis.conf:/etc/init.d/redis-server restart"
+/usr/bin/hipache --config /data/router/hipache.conf
