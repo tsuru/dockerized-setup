3a9b54844919cc4e6ba1bdaa3c0a0cc72f4ceabe
Wagner Souza <wagnersza@corp.globo.com>
2015-08-27 11:13:35 -0300

    gandalf/run.sh: added sshd and provisionally rsyslogd

--- gandalf/run.sh
+++ gandalf/run.sh
@@ -5,7 +5,13 @@ TSURU_TOKEN=`curl -s http://consul.service.consul:8500/v1/kv/tsuru/token?raw`
 if [ "$TSURU_TOKEN" == "" ]; then
   TSURU_TOKEN=`/bin/docker run -v /data/tsuru:/data/tsuru tsuru/tsuru-api token --config=/data/tsuru/tsuru.conf`
   curl -s -X PUT -d $TSURU_TOKEN http://consul.service.consul:8500/v1/kv/tsuru/token
+  /bin/docker run -v /data/tsuru:/data/tsuru tsuru/tsuru-api gandalf-sync --config=/data/tsuru/tsuru.conf
+  /usr/sbin/sshd -D &
+  /usr/sbin/rsyslogd &
   /usr/bin/gandalf-server -config=/data/gandalf/gandalf.conf
 else
+  /bin/docker run -v /data/tsuru:/data/tsuru tsuru/tsuru-api gandalf-sync --config=/data/tsuru/tsuru.conf
+  /usr/sbin/sshd -D &
+  /usr/sbin/rsyslogd &
   /usr/bin/gandalf-server -config=/data/gandalf/gandalf.conf
 fi
