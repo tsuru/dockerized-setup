8d3150d39bbae1a793807e0b3dc5d4ba284ddc87
Colin Saliceti <c.saliceti@gmail.com>
2015-10-23 00:33:11 +0100

    Move git chown to run.sh
    These files are on the external mounted volume. We can’t change ownership at image build time.

--- gandalf/run.sh
+++ gandalf/run.sh
@@ -2,6 +2,8 @@
 
 TSURU_TOKEN=`curl -s http://consul.service.consul:8500/v1/kv/tsuru/token?raw`
 
+chown -R git:git /data/gandalf
+
 if [ "$TSURU_TOKEN" == "" ]; then
   TSURU_TOKEN=`/bin/docker run -v /data/tsuru:/data/tsuru tsuru/tsuru-api token --config=/data/tsuru/tsuru.conf`
   curl -s -X PUT -d $TSURU_TOKEN http://consul.service.consul:8500/v1/kv/tsuru/token
